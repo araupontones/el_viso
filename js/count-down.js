5e39384b2a568f11af82ddd1f043a6c5b30cf1a9
araupontones <43657130+araupontones@users.noreply.github.com>
2022-10-31 11:53:51 +0100

    fix count-down

--- js/count-down.js
+++ js/count-down.js
@@ -1,11 +1,11 @@
 var raceDate = new Date("April 1, 2023 9:30:00");
 var today = new Date().getTime();
-var distance = raceDate - today;
+var distance = raceDate - today ;
 
-var days = Math.floor(distance / (1000 * 60 * 60 * 24)) ;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24)) + 1  ;
 
 
-
+//turn 0 if the today is greater that the race
 if (days  < 0) {
 
   var days = 0
