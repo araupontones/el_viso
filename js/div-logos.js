c8aac7828c7df12c7c42f89789d002762afacd96
araupontones <43657130+araupontones@users.noreply.github.com>
2022-11-11 11:09:08 +0100

    js logos

---
+++ js/div-logos.js
@@ -0,0 +1,27 @@
+let logos = ["logo-cinco-villas.svg",
+"uncastillo.png",
+"banales.jpg",
+"cedres.JPG",
+"celma.jpg",
+"ciclos_aragon.png",
+"equipaziones2.png",
+"frago.jpg",
+"olnasa.jpg",
+"peman.png",
+"podoactiva.png",
+"pulpodata.png",
+"una.jpg",
+
+];
+
+
+
+  console.log(source);
+
+  for (let i = 0; i < logos.length; i++) {
+      console.log(logos[i]);
+      l = logos[i];
+      var source = "imgs/logos/" + l;
+      //$('#logos').append('<div>hola</div>');
+      $('#logos').append(  '<div class="col-6 col-md-3 my-auto">  <img class="img-fluid logo" src="'+source+'" alt=""></div>');
+  }
